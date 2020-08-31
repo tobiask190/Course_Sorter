a343a3850644e23bb489b1b535f72ef06721c422
Alex Chen <achen115@ucr.edu>
2020-08-31 01:46:05 -0700

    implement course loading

---
+++ Course_Loader/Program.cs
@@ -0,0 +1,231 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Course_Loader {
+    enum Hours {
+        _0800, _0830,
+        _0900, _0930,
+        _1000, _1030,
+        _1100, _1130,
+        _1200, _1230,
+        _1300, _1330,
+        _1400, _1430,
+        _1500, _1530,
+        _1600, _1630,
+        _1700, _1730,
+        _1800, _1830,
+        _1900, _1930,
+        _2000, _2030,
+    }
+    enum SectionType {
+        Unknown,
+        Lecture,
+        Discussion,
+        Laboratory,
+        Studio,
+        Workshop,
+        Seminar,
+        Individualized_Study,
+        Research,
+        Field,
+        Consultation,
+        Class,
+        Written_Work,
+        Practicum,
+        Internship,
+        Colloquium,
+        Clinic,
+        Screening,
+        Tutorial,
+        Thesis,
+        Activity,
+        Term_Paper,
+        Additional_Lecture,
+        XXX
+    }
+    class Course {
+        public int crn;
+        public string subject;
+        public string courseNumber;
+        public string sequenceNumber;
+        public string title;
+        public string units;
+        public HashSet<DayOfWeek> days;
+
+        //Convert this to Hours later?
+        public string time;
+
+        public SectionType type;
+        public string building;
+        public string room;
+        public string startDate;
+        public string endDate;
+        public string instructor;
+        public string available;
+        public string reserved;
+    }
+    class Program {
+        static void Main(string[] args) {
+            HashSet<string> errors = new HashSet<string>();
+
+            HashSet<Course> courses = new HashSet<Course>();
+
+            foreach(var f in Directory.GetFiles(@"C:\Users\alexm\source\repos\Course_Sorter\Course_Reader\bin\Debug\netcoreapp3.1", "*.html")) {
+                HtmlDocument d = new HtmlDocument();
+                d.LoadHtml(File.ReadAllText(f).Replace("&nbsp;", ""));
+                var table = d.GetElementbyId("table1").SelectSingleNode("./tbody");
+                //Each entry is a <tr> stored in the <tbody> with attributes specified in <td>
+                foreach(var courseRow in table.ChildNodes) {
+                    int crn = 0;
+                    string subject = null;
+                    string courseNumber = null;
+                    string sequenceNumber = null;
+                    string title = null;
+                    string units = null;
+                    HashSet<DayOfWeek> days = null;
+
+                    //Convert this to Hours later?
+                    string time = null;
+                    
+                    SectionType type = 0;
+                    string building = null;
+                    string room = null;
+                    string startDate = null;
+                    string endDate = null;
+                    string instructor = null;
+                    string available = null;
+                    string reserved = null;
+                        foreach (var td in courseRow.SelectNodes("./td")) {
+                            string text = td.InnerText;
+                            switch (td.GetAttributeValue("data-property", null)) {
+                                case "courseReferenceNumber":
+                                    crn = int.Parse(text);
+                                    break;
+                                case "subject":
+                                    subject = text;
+                                    break;
+                                case "courseNumber":
+                                    courseNumber = text;
+                                    break;
+                                case "sequenceNumber":
+                                    sequenceNumber = text;
+                                    break;
+                                case "courseTitle":
+                                    title = text;
+                                    break;
+                                case "creditHours":
+                                    units = text;
+                                    break;
+                                case "meetingTime":
+                                    var meeting = td.FirstChild;
+
+                                    //Some courses (ARC) have no meeting time
+                                    if (meeting == null) {
+                                        continue;
+                                    }
+
+
+                                    text = meeting.FirstChild.FirstChild.InnerText;
+                                    if (text == "None") {
+                                        days = new HashSet<DayOfWeek>();
+                                    } else {
+                                        days = text.Split(",").Select(day => Enum.Parse<DayOfWeek>(day)).ToHashSet();
+                                    }
+
+                                    time = meeting.ChildNodes[1].InnerText;
+
+                                    text = meeting.ChildNodes[2].InnerText;
+
+                                    type = Enum.Parse<SectionType>(text.Substring(text.IndexOf(':') + 1).Replace(" ", "_"));
+
+                                    text = meeting.ChildNodes[3].InnerText;
+                                    building = text.Substring(text.IndexOf(':') + 1);
+
+                                    text = meeting.ChildNodes[4].InnerText;
+                                    room = text.Substring(text.IndexOf(':') + 1);
+
+                                    startDate = meeting.ChildNodes[5].InnerText;
+                                    endDate = meeting.ChildNodes[6].InnerText;
+                                    break;
+                                case "instructor":
+                                    instructor = text;
+                                    break;
+                                case "status":
+                                    available = text;
+                                    break;
+                                case "reserved":
+                                    reserved = text;
+                                    break;
+                                case null:
+                                    //End of <tr>
+                                    break;
+                            }
+                        }
+                    
+                    courses.Add(new Course() {
+                        crn = crn,
+                        subject = subject,
+                        courseNumber = courseNumber,
+                        sequenceNumber = sequenceNumber,
+                        title = title,
+                        units = units,
+                        days = days,
+                        time = time,
+                        type = type,
+                        building = building,
+                        room = room,
+                        startDate = startDate,
+                        endDate = endDate,
+                        instructor = instructor,
+                        available = available,
+                        reserved = reserved
+
+                    });
+                }
+
+                /*
+                foreach(var line in File.ReadAllLines(f)) {
+                    if(line.Contains("table1")) {
+                        var start = line.IndexOf("<table");
+                        var s = "</table>";
+                        var end = line.IndexOf(s) + s.Length;
+
+                        s = line.Replace("&", "&amp;").Replace("<br>", "").Substring(start, end - start);
+                        var doc = XDocument.Parse(s);
+                        break;
+                    }
+                }
+                //var doc = XDocument.Parse($"<Root>{File.ReadAllText(f)}</Root>");                
+                */
+
+                /*
+                foreach(var n in GetNodes(doc.Root)) {
+                    if(n.Attribute("id")?.Value == "table1") {
+                        
+                        break;
+                    }
+                }
+                */
+                /*
+                IEnumerable<XElement> GetNodes(XElement root) {
+                    var nodes = new Queue<XElement>();
+                    nodes.Enqueue(root);
+                    while(nodes.Any()) {
+                        var e = nodes.Dequeue();
+                        yield return e;
+                        foreach(var c in e.Elements()) {
+                            nodes.Enqueue(c);
+                        }
+                    }
+                }
+                */
+            }
+            return;
+        }
+    }
+}
---
+++ Course_Reader/Program.cs
@@ -0,0 +1,96 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace Course_Reader {
+    class Program {
+        static void Main(string[] args) {
+            IWebDriver driver = new ChromeDriver();
+            try {
+                // Navigate to Url
+                driver.Navigate().GoToUrl("https://registrationssb.ucr.edu/StudentRegistrationSsb/ssb/term/termSelection?mode=search");
+
+                Action<Action<Actions>> Perform = perform => {
+                    var a = new Actions(driver);
+                    perform(a);
+                    a.Build().Perform();
+                };
+
+
+                var f = driver.FindElement(By.Id("s2id_txt_term"));
+                Perform(a => a.Click(f));
+
+                Thread.Sleep(1000);
+
+                //Click the first available term
+                Perform(a => a.MoveToElement(f).MoveByOffset(0, 60).Click());
+                
+                Thread.Sleep(200);
+
+                Perform(a => a.Click(driver.FindElement(By.Id("term-go"))));
+                Thread.Sleep(1000);
+
+                int t = 6000;
+                void EnterSearch() {
+                    Perform(a => a.Click(driver.FindElement(By.Id("search-go"))));
+                    Thread.Sleep(2000);
+
+                    //Find the entries per page field by the label
+                    var perPage = driver.FindElement(By.CssSelector("[aria-label=\"10 per page\"]"));
+                    Perform(a => a.Click(perPage));
+
+                    //Menu appears above the field, so arrow down and enter 50
+                    Perform(a => a.SendKeys(Keys.Down));
+                    Perform(a => a.SendKeys(Keys.Down));
+                    Perform(a => a.SendKeys(Keys.Down));
+                    Perform(a => a.SendKeys(Keys.Enter));
+
+                    Thread.Sleep(t);
+                }
+
+                void SetPage(int page) {
+                    Perform(a => a.SendKeys(Keys.Tab));
+                    Perform(a => a.SendKeys(Keys.Tab));
+                    Perform(a => a.SendKeys(Keys.Tab));
+                    Perform(a => a.SendKeys(Keys.Tab));
+                    Perform(a => a.SendKeys((page).ToString()));
+                    Perform(a => a.SendKeys(Keys.Enter));
+
+                    //Find the button by its tooltip
+                    //Perform(a => a.MoveToElement(driver.FindElement(By.XPath("//*[@title=\"Next\"]"))).Click());
+                    Thread.Sleep(t);
+                }
+
+                EnterSearch();
+
+                //Find the button by its tooltip
+                //Perform(a => a.MoveToElement(driver.FindElement(By.XPath("//*[@title=\"Last\"]"))).Click());
+                Thread.Sleep(t);
+
+                int start = 179;
+
+                SetPage(start);
+                for (int i = start; i < 210; i++) {
+                    if(i%10 == 0) {
+                        driver.Navigate().GoToUrl("https://registrationssb.ucr.edu/StudentRegistrationSsb/ssb/classSearch/classSearch");
+                        EnterSearch();
+                        SetPage(i);
+                    }
+
+                    File.WriteAllText($"page{i}.html", driver.PageSource);
+
+                    //Find the button by its tooltip
+                    Perform(a => a.MoveToElement(driver.FindElement(By.XPath("//*[@title=\"Next\"]"))).Click());
+                    Thread.Sleep(t);
+                }
+
+                //Perform(a => a.MoveToElement(perPage.FindElements(By.TagName("option"))[3]).Click());
+            } finally {
+                driver.Quit();
+            }
+        }
+    }
+}
